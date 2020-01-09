c11fd96d7e2fcb85da37e433f2fd97d024d4b82c
Denperidge <denperidge@gmail.com>
2020-01-09 19:31:01 +0100

    Added debug to .env

--- lgsmcp/lgsmcp/settings.py
+++ lgsmcp/lgsmcp/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# If anything of debug is defined, set to debug
+DEBUG = bool(os.environ.get('DEBUG', False))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
