ff2c66da4ac7af6baf2e6c28eb91e4b16823a382
Denperidge <denperidge@gmail.com>
2020-01-11 00:01:22 +0100

    start.py sudo session start before daemon popen

--- start.py
+++ start.py
@@ -3,8 +3,12 @@ from os import system
 from os import path
 
 currentDir = path.abspath(__file__).replace(path.basename(__file__), "")
-try: 
 
+# Call echo with sudo so that the sudo password won't be asked for the daemon
+# (as the input gets muddled and confusing with the server launches)
+subprocess.check_output(["sudo", "echo", "\"Starting lgsmcp!\""])
+
+try:
     server = subprocess.Popen(["python3", currentDir + "/lgsmcp/manage.py", "runserver", "--noreload", "0.0.0.0:8000"])
     daemon = subprocess.Popen(["sudo", "python3", currentDir + "/daemon/daemon.py"])
     daemon.communicate()
